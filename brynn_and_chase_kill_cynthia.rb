352b6b0c4adbdab384f96ae0b2b72b3e453375a2
Jakob Persons <jakobep24@gmail.com>
2020-10-15 21:01:51 +0000

    Automatically backed up by Learn

---
+++ brynn_and_chase_kill_cynthia.rb
@@ -0,0 +1,10 @@
+stabbings = 0
+
+cynthia_dies = 24
+
+while stabbings <= cynthia_dies do
+  puts "Brynn and Chase have stabbed Cynthia #{stabbings} times, but she is still alive"
+  stabbings += 1
+end
+
+puts "Cynthia is dead, the world is a better place."
